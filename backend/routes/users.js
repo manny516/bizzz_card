674f66ef5d796a856d29056137d0617e7ef4950d
manny516 <mwalthrust@gmail.com>
2022-04-24 10:30:19 -0400

    updated users route / added find by id , delete by id, and update by id

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -22,7 +22,7 @@ router.get("/:id", (req,res)=>{
 
 
 router.post("/add",jsonParse, async (req,res) =>{
-    console.log(JSON.stringify(req.body));
+
     const first_name = req.body.first_name;
     const last_name = req.body.last_name;
     const company = req.body.company;
@@ -57,13 +57,48 @@ router.post("/add",jsonParse, async (req,res) =>{
     .catch(err => res.status(400).json('Error: ' + err ))
 });
 
-router.delete("/remove", async(req,res)=>{
+router.post("/update/:id", async (req,res)=>{
+    bizzUser.findById(req.params.id)
+    .then(bizzzCards =>{
+        bizzzCards.first_name = req.body.first_name;
+        bizzzCards.last_name = req.body.last_name;
+        bizzzCards.company = req.body.company;
+        bizzzCards.phone = req.body.phone;
+        bizzzCards.email = req.body.email;
+        bizzzCards.urls = req.body.urls;
+        bizzzCards.address = req.body.address;
+        bizzzCards.birthday = req.body.birthday;
+        bizzzCards.posted = req.body.posted;
+        bizzzCards.updated = Date.parse(req.body.date);
+        bizzzCards.social_links = req.body.social_links;
+        bizzzCards.instant_messagers = req.body.instant_messagers;
+        bizzzCards.notes = req.body.notes;
+
+        bizzzCards.save()
+        .then(() => res.json('User Updated'))
+        .catch( err => res.status(400).json("Error:" + err))
+    })
+    .catch(err => res.status(400).json('Errors' + err))
+});
+
+router.delete("/remove/:id", async(req,res)=>{
     try{
-        const removeRecord = await bizzUser.deleteOne(itemObj);
+        const removeRecord = await bizzUser.findByIdAndDelete(req.params.id);
         console.log(removeRecord);
     }catch(e){
         console.log(e.message);
     }
 
-})
+});
+
+// router.delete("/remove/", jsonParse, async(req,res)=>{
+//     const idList = req.body.idList;
+//     try{
+//         const removeRecord = await bizzUser.deleteMany(idList);
+//         console.log(removeRecord);
+//     }catch(e){
+//         console.log(e.message);
+//     }
+
+// })
 module.exports = router;
