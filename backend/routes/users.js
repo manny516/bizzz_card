f116da1247d724a2cb4768d734d49d46a81e9a02
manny516 <mwalthrust@gmail.com>
2022-05-02 02:54:32 -0400

    setting up PATCH route for user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -82,6 +82,32 @@ router.put("/update/:id", jsonParse, async (req,res)=>{
     .catch(err => res.status(400).json('Errors' + err))
 });
 
+router.patch("/edit/:id", jsonParse, async (req,res)=>{
+    bizzUser.findById(req.params.id)
+    .then(async bizzzCards =>{
+        bizzzCards.id = req.params._id;
+        bizzzCards.first_name = req.body.first_name;
+        bizzzCards.last_name = req.body.last_name;
+        bizzzCards.company = req.body.company;
+        bizzzCards.phone = req.body.phone;
+        bizzzCards.email = req.body.email;
+        bizzzCards.urls = req.body.urls;
+        bizzzCards.address = req.body.address;
+        bizzzCards.birthday = req.body.birthday;
+        bizzzCards.posted = req.body.posted;
+        bizzzCards.updated = Date.parse(req.body.date);
+        bizzzCards.social_links = req.body.social_links;
+        bizzzCards.instant_messagers = req.body.instant_messagers;
+        bizzzCards.notes = req.body.notes;
+
+       await  bizzzCards.save()
+        .then(() => res.json('User Updated'))
+        .catch( err => res.status(400).json("Error:" + err))
+    })
+    .catch(err => res.status(400).json('Errors' + err))
+});
+
+
 router.delete("/remove/:id", async(req,res)=>{
     try{
         const removeRecord = await bizzUser.findByIdAndDelete(req.params.id);
---
+++ frontend/.next/static/NXieSmj3nQxrpVzsHIj6W/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9cf0cd0c3d3cdc8f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
