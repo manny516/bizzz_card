85704b71b5900d22cbca576bc49c3a109bc7ee89
manny516 <mwalthrust@gmail.com>
2022-05-20 03:18:05 -0400

    added avatar / background image / for users profile

--- frontend/src/pages/users/[slug].tsx
+++ frontend/src/pages/users/[slug].tsx
@@ -1,23 +1,31 @@
 import type { NextPage } from 'next'
 import  MetaHead  from "components/globals/MetaHead";
 import { useRouter } from "next/router";
-import Imgix, { ImgixProvider } from "react-imgix";
+import Image from 'next/image'
+import profileImg from "img/profile.png";
+import DividerBlind from 'components/globals/DividerBlind';
 
 const Profile: NextPage = () => {
-   const router = useRouter();
-    let slug = router.query.slug;
+  const router = useRouter();
+  let slug = router.query.slug;
+
 
   return (
     <>
         <MetaHead/>
-        <section className='card-bg w-screen h-80 overflow-hidden'>
-       <h1>
-          Welcome User {slug}
-        </h1>
-            <Imgix src="https://assets.imgix.net/examples/pione.jpg" sizes="100%" />;
+        <section className='dark-mode pt-10 blur-box card-bg w-screen h-80 overflow-hidden' style={{backgroundImage :'url(https://assets.imgix.net/examples/pione.jpg)', backgroundSize:"cover", backgroundRepeat:"no-repeat"}}>
+            <article className=' w-28 m-auto'>
+                <Image className='rounded-full m-auto' src={profileImg} width="100" height="100"/>
+                <h1>{slug}</h1> 
+            </article>
         </section>
+        <DividerBlind classList="h-72 relative -top-72 z-40 " />
       </>
   )
 }
 
 export default Profile
+
+
+
+
